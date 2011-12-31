f5593fdc633e8ff9c86fd9528ecf640fed67a08b
Kevin Strong <kmstrong@san.rr.com>
2011-12-30 18:43:23 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+
+var PORT = 1337
+var io = require('socket.io').listen(PORT);
+
+console.log('listening on http://localhost:' + PORT + '/ ...');
+
+var MAX_LEN = 4096;
+
+var names = [];
+var msg_history = [];
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('history', { msg: msg_history });
+
+	socket.on('join', function (message) {
+		socket.set('name', message.name, function () {
+			names.push(message.name);
+			io.sockets.emit('names', {names: names});
+		});
+	});
+
+	socket.on('name change', function (message) {
+		if (names.indexOf(message.newname) == -1) {
+			socket.set('name', message.newname, function () {
+				var oldidx;
+				if ((oldidx = names.indexOf(message.oldname)) != -1) {
+					names.splice(oldidx, 1);
+				}
+				names.push(message.newname);
+				io.sockets.emit('names', {names: names});
+				socket.emit('name change ok', message);
+			});
+		}
+		else {
+			socket.emit('name taken');
+		}
+	});
+
+	socket.on('disconnect', function () {
+		socket.get('name', function (err, name) {
+			var oldidx;
+			if ((oldidx = names.indexOf(name)) != -1) {
+				names.splice(oldidx, 1);
+			}
+			io.sockets.emit('names', {names: names});
+		});
+	});
+
+	socket.on('msg', function (message) {
+		if (message.msg && message.msg.length <= MAX_LEN) {
+			msg_history.push(message);
+			msg_history = msg_history.slice(-200);
+			io.sockets.emit('chat', message);
+		}
+		else {
+			socket.emit('stfu');
+		}
+	});
+});
